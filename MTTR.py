52b45d6da51cc97a032667d45aac371574f45a80
mihail <extrude@yandex.ru>
2018-07-17 16:43:45 +0200

    Add function index_row

--- MTTR.py
+++ MTTR.py
@@ -5,19 +5,32 @@ from datetime import datetime
 from datetime import timedelta
 
 template_mttr = '''
-            Кол-во инфраструктурных инцидентов: {}
-            MTTR                                {}
-            '''
+Кол-во инфраструктурных инцидентов: {}
+MTTR                                {}
+'''
+
+def index_row(line):
+    l = line.strip().split(';')
+    print(line)
+    return l.index('Номер'), l.index('Время направления в работу'), l.index('Время закрытия события в системе мониторинга')
+    
+
+
+
 
 with open('export.csv', 'r') as f:
     sum_time_all_incident = 0
     count_incident = 0
+    first_line = True
     for line in f:
-        incident = line.strip().split(';')
-        if 'ИНЦ' in incident[0]:
+        if first_line:
+            index_num_incident, index_time_work, index_close = index_row(line)
+            first_line = False
+        else:
+            incident = line.strip().split(';')
             count_incident += 1
-            time_ref_to_work = datetime.strptime(incident[1], '%d.%m.%Y %H:%M')
-            time_close_monit_system = datetime.strptime(incident[2], '%d.%m.%Y %H:%M')
+            time_ref_to_work = datetime.strptime(incident[index_time_work], '%d.%m.%Y %H:%M')
+            time_close_monit_system = datetime.strptime(incident[index_close], '%d.%m.%Y %H:%M')
             sum_time_all_incident = sum_time_all_incident + (time_close_monit_system - time_ref_to_work).total_seconds()
     print(template_mttr.format(count_incident, int((sum_time_all_incident / 60) / count_incident)))
 

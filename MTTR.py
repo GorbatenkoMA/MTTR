47a29f449882c83ad3145428692460af9686d2a6
mihail <extrude@yandex.ru>
2018-08-31 08:12:11 +0200

    Add convert file ansi to utf-8 Add def find last file in directory

--- MTTR.py
+++ MTTR.py
@@ -1,9 +1,14 @@
 #!/root/venv/my_env/bin/python3.6
 #-*- coding: utf-8 -*-
 
+# mount -t cifs //10.10.1.141/mttr /mnt/mttr -o user=cisco,password=cisco 
+
 from datetime import datetime
 from datetime import timedelta
 from operator import itemgetter
+import os
+import glob
+import codecs
 
 template_mttr = '''
 Кол-во инфраструктурных инцидентов: {}
@@ -11,7 +16,6 @@ template_mttr = '''
 Суммарное время простоя, мин:       {} 
 MTTR расчитанное, мин:              {}
 Итоговое значение КПЭ %:            {}
-Необходимое кол-во инцидентов для достижения целевого показателя: {}
 '''
 dict_mttr = {'count_incident': None,
            'target_mttr': None,
@@ -25,12 +29,22 @@ def index_row(line):
     l = line.strip().split(';')
     return l.index('Номер'), l.index('Время направления в работу'), l.index('Время закрытия события в системе мониторинга')
 
+def last_modified_file(folder):
+    files = list(filter(os.path.isfile, glob.glob(folder + "/*")))
+    if len(files) > 0:
+        files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+        return files[0]
+    else:
+        return None
+
 def calc_mttr(espp_file = 'export.csv', target_mttr = 130):
-    with open(espp_file, 'r') as f:
+#    with open(espp_file, 'r') as f:
+    with codecs.open(espp_file, 'r', encoding='windows-1251') as f:
         sum_time_all_incident = 0
         count_incident = 0
         first_line = True
         for line in f:
+            #print(line)
             if first_line:
                 index_num_incident, index_time_work, index_close = index_row(line)
                 first_line = False
@@ -48,11 +62,11 @@ def calc_mttr(espp_file = 'export.csv', target_mttr = 130):
     dict_mttr['sum_time_all_incident'] = int(sum_time_all_incident)
     dict_mttr['mttr'] = int(sum_time_all_incident / count_incident)
     dict_mttr['kpe'] = int((target_mttr / (sum_time_all_incident / count_incident)) * 100)
-    dict_mttr['count_incident_to_target_mttr'] = int((count_incident * target_mttr) / (sum_time_all_incident / count_incident) + count_incident)
+    #dict_mttr['count_incident_to_target_mttr'] = int((count_incident * target_mttr) / (sum_time_all_incident / count_incident) + count_incident)
 
     return dict_mttr
 
-d = calc_mttr()
+d = calc_mttr(last_modified_file('/mnt/mttr/'))
 
 print(template_mttr.format(d['count_incident'], d['target_mttr'], d['sum_time_all_incident'], d['mttr'], d['kpe'], d['count_incident_to_target_mttr']))
 
